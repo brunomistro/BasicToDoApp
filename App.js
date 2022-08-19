ab67d47865716c3964999e3211b3182b46415efc
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2022-08-19 13:48:35 -0300

    First Commit

--- App.js
+++ App.js
@@ -1,12 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, StyleSheet, View, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+
+//Components
+import Header from "./components/header"
+import TodoItem from "./components/todoItem"
+import AddTodo from './components/addTodo';
 
 export default function App() {
+	const [todos, setTodos] = useState([
+		{ text: "buy coffe", key: 1},
+		{ text: "create app", key: 2},
+		{ text: "study", key: 3},
+	]);
+
+	const pressHandler = (key) => {
+		setTodos(prev => {
+			return prev.filter(item => item.key != key);
+		});
+	};
+
+	const submitHandler = (text) => {
+		if(text.length > 3){
+			setTodos((prev) => {
+				return [
+					...prev,
+					{text, key: Math.random().toString() },
+				]
+			});
+		}
+		else {
+			Alert.alert("OPS", "Item must be over 3 characters", [
+				{ text: "OK!" }
+			])
+		}
+	}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+		<TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+			<View style={styles.container}>
+				<StatusBar style="auto" />
+				
+				<Header />
+				<View style={styles.content}>
+					<AddTodo submitHandler={submitHandler} />
+
+					<FlatList
+						data={todos}
+						renderItem={({item}) => (
+							<TodoItem item={item} pressHandler={pressHandler} />
+						)}
+					/>
+				</View>
+			</View>
+		</TouchableWithoutFeedback>
   );
 }
 
@@ -14,7 +61,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+	content: {
+		padding: 40,
+	},
+	list: {
+		marginTop: 20,
+	},
 });
---
+++ README.md
@@ -0,0 +1,2 @@
+## Basic To Do App
+#### basic app for an college assignment, that's it
